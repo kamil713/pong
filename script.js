c2099211e66ed7643a1adace2b08f1b85b47c315
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-09-14 23:25:00 +0200

    feat: added ai

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const ballSize = 20;
 let ballX = cw / 2 - ballSize / 2;
 let ballY = ch / 2 - ballSize / 2;
 
-const paddelHeight = 100;
-const paddelWidth = 20;
+const paddleHeight = 100;
+const paddleWidth = 20;
 
 const playerX = 70;
 const aiX = 910;
@@ -28,12 +28,12 @@ let ballSpeedY = 4;
 
 function player() {
 	ctx.fillStyle = '#7FFF00';
-	ctx.fillRect(playerX, playerY, paddelWidth, paddelHeight);
+	ctx.fillRect(playerX, playerY, paddleWidth, paddleHeight);
 }
 
 function ai() {
 	ctx.fillStyle = 'yellow';
-	ctx.fillRect(aiX, aiY, paddelWidth, paddelHeight);
+	ctx.fillRect(aiX, aiY, paddleWidth, paddleHeight);
 }
 
 function ball() {
@@ -69,11 +69,11 @@ function table() {
 let topCanvas = canvas.offsetTop;
 
 function playerPosition(e) {
-	playerY = e.clientY - topCanvas - paddelHeight / 2;
+	playerY = e.clientY - topCanvas - paddleHeight / 2;
 
-	if (playerY > ch - paddelHeight) {
+	if (playerY > ch - paddleHeight) {
 		// >=
-		playerY = ch - paddelHeight;
+		playerY = ch - paddleHeight;
 	}
 
 	if (playerY < 0) {
@@ -88,21 +88,53 @@ function playerPosition(e) {
 function speedUp() {
 	// Speed X
 	if (ballSpeedX > 0 && ballSpeedX < 16) {
-		ballSpeedX += 0.4;
+		ballSpeedX += 0.2;
 	} else if (ballSpeedX < 0 && ballSpeedX > -16) {
-		ballSpeedX -= 0.4;
+		ballSpeedX -= 0.2;
 	}
 
 	// Speed Y
 	if (ballSpeedY > 0 && ballSpeedY < 16) {
-		ballSpeedY += 0.4;
+		ballSpeedY += 0.2;
 	} else if (ballSpeedY < 0 && ballSpeedY > -16) {
-		ballSpeedY -= 0.4;
+		ballSpeedY -= 0.2;
 	}
 }
 
 // AI
-function aiPosition() {}
+function aiPosition() {
+  const middlePaddle = aiY + paddleHeight / 2;
+  const middleBall = ballY + ballSize / 2;
+
+  if (ballX > 500) {
+    if(middlePaddle - middleBall > 200) {
+      aiY -= 24;
+    } else if (middlePaddle - middleBall > 50) {
+      aiY -= 10;
+    } else if (middlePaddle - middleBall < -200) {
+      aiY += 24;
+    } else if (middlePaddle - middleBall < -50) {
+      aiY += 10;
+    }
+  }
+
+  if (ballX <= 500 && ballX > 100) {
+    if (middlePaddle - middleBall > 100) {
+      aiY -= 3;
+    }
+    if (middlePaddle - middleBall < -100) {
+      aiY += 3;
+    }
+  }
+
+  if (aiY >= ch - paddleHeight) {
+    aiY = ch - paddleHeight;
+  }
+
+  if (aiY <= 0) {
+    aiY = 0;
+  }
+}
 
 canvas.addEventListener('mousemove', playerPosition);
 
